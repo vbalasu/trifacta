52fc974aeb15188b0ce2fbf8bf09eab8f0dc9af4
Vijay Balasubramaniam <vbalasu@gmail.com>
2018-07-22 10:16:39 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='trifacta',
+    version='1.0',
+    packages=['trifacta',],
+    license='UNLICENSED',
+    long_description=open('README.md').read(),
+)
