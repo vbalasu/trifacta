0dbc40c702913609eaec9e9cc22418d982f377ff
Vijay Balasubramaniam <vbalasu@gmail.com>
2018-07-22 10:07:54 -0700

    Corrected trifacta.py

--- trifacta.py
+++ trifacta.py
@@ -1,41 +1,57 @@
-# Trifacta List Lobs
-import requests
-import base64
 
-def list_jobs(baseurl = "http://partnerdemo.trifacta.net:3005", user_id = 'demo@trifacta.com', password = 'demo'):
-    url = baseurl + '/v3/jobGroups'
-    authorization = 'Basic ' + base64.b64encode(str.encode(user_id + ':' + password)).decode('utf-8')
-    headers = {
-        'Authorization': authorization,
-        'Cache-Control': "no-cache"
-        }
-    return requests.request("GET", url, headers=headers).text
+# coding: utf-8
 
-# print(list_jobs())
+# # trifacta
+# 
+# 1. Connect to a remote Trifacta instance
+# 2. Run jobs
+# 3. Send and receive files
 
-# Trifacta Run Job
-def run_job(wrangled_dataset_id = 5184, baseurl = 'http://partnerdemo.trifacta.net:3005', user_id = 'demo@trifacta.com', password = 'demo'):
-    url = baseurl + "/v3/jobGroups"
-    authorization = 'Basic ' + base64.b64encode(str.encode(user_id + ':' + password)).decode('utf-8')
-    payload = "{\n  \"wrangledDataset\": {\n\"id\": " + str(wrangled_dataset_id) + " }\n}"
-    headers = {
-        'Content-Type': "application/json",
-        'Authorization': authorization,
-        'Cache-Control': "no-cache"
-        }
-    return requests.request("POST", url, data=payload, headers=headers).text
+# In[1]:
 
-# print(run_job(wrangled_dataset_id = 5184))
 
-# Trifacta Get Job Status
-def get_job_status(job_group_id, baseurl = 'http://partnerdemo.trifacta.net:3005', user_id = 'demo@trifacta.com', password = 'demo'):
-    url = baseurl + "/v3/jobGroups/" + str(job_group_id) + "/status"
-    authorization = 'Basic ' + base64.b64encode(str.encode(user_id + ':' + password)).decode('utf-8')
-    headers = {
-        'Authorization': authorization,
-        'Cache-Control': "no-cache"
-        }
-    return requests.request("GET", url, headers=headers).text
+#!pip install pywebhdfs
 
-# print(get_job_status(1501))
+
+# In[2]:
+
+
+import requests, json, time, datetime, io
+from pywebhdfs.webhdfs import PyWebHdfsClient
+from urllib.parse import urlparse
+import pandas as pd
+class Trifacta:
+    def __init__(self, trifacta_base_url, user_id, password):
+        """Example: t = Trifacta('https://partnerdemo.trifacta.net', 'guest_user', 'guest_password')"""
+        self.trifacta_base_url = trifacta_base_url
+        self.user_id = user_id
+        self.password = password
+    def get_job_status(self, job_group_id):
+        response = requests.get(self.trifacta_base_url + '/v3/jobGroups/{0}/status'.format(job_group_id), auth=('admin@trifacta.local', 'Tr1f@ct@1!'))
+        return json.loads(response.text)
+    def run_job(self, wrangled_dataset_id):
+        """Get the wrangled_dataset_id from your browser's URL when editing the recipe"""
+        request_body = {"wrangledDataset": {"id": wrangled_dataset_id }}
+        print('About to run job', flush=True)
+        response = requests.post(self.trifacta_base_url + '/v3/jobGroups', auth=(self.user_id, self.password), json=request_body)
+        response_obj = json.loads(response.text)
+        print(response_obj, flush=True)
+        self.job_group_id = response_obj['jobgroupId']
+        job_status = self.get_job_status(self.job_group_id)
+        while job_status != 'Complete':
+            print(datetime.datetime.today(), job_status, flush=True)
+            time.sleep(5)
+            job_status = self.get_job_status(self.job_group_id)
+        print(datetime.datetime.today(), job_status, flush=True)
+        return True
+    def get_file_contents(self, hdfs_path, user_name='trifacta', httpfs_port='14000'):
+        hdfs = PyWebHdfsClient(host=urlparse(self.trifacta_base_url).netloc,port=httpfs_port, user_name=user_name)
+        return hdfs.read_file(hdfs_path).decode('utf-8')
+    def get_dataframe(self, hdfs_path_to_csv):
+        csv_string = self.get_file_contents(hdfs_path_to_csv)
+        return pd.read_csv(io.StringIO(csv_string))
+    def put_file_contents(self, hdfs_path, file_contents, user_name='trifacta', httpfs_port='14000'):
+        hdfs = PyWebHdfsClient(host=urlparse(self.trifacta_base_url).netloc,port=httpfs_port, user_name=user_name)
+        hdfs.create_file(hdfs_path, file_contents, overwrite=True)
+        return True
 
